c4123d828ad42099736808ac66ffc4f7fd2edf39
vindyag <vindyag@99x.lk>
2021-08-12 17:05:19 +0530

    add synchronized

--- src/main/java/demo/synchronization/Calculator.java
+++ src/main/java/demo/synchronization/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
     int cell2;
     int cell3;
 
-    int setAndGetSum(int val1, int val2, int val3) throws InterruptedException {
+    synchronized int setAndGetSum(int val1, int val2, int val3) throws InterruptedException {
         System.out.println("Starting " + Thread.currentThread().getName());
         cell1 = val1;
         System.out.println(Thread.currentThread().getName() + " setting cell1 " + cell1);
